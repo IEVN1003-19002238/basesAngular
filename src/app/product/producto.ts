59bebf12f9deb22e8ac1bac97674036e9b35823d
Jorge Urbina <alb15.urbi@gmail.com>
2024-09-18 18:59:14 -0600

    18/09/2024 operaciones basicas

---
+++ src/app/product/producto.ts
@@ -0,0 +1,10 @@
+export interface IProducto {
+      productoId:number;
+      modelo: string;
+      Descripcion: string;
+      year:string;
+      precio: number;
+      Marca:string;
+      Color:string;
+      imagenUrl:string;
+}
