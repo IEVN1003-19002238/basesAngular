de72a65ac0f54210f98fd1ff8f8e3b2b2c76108e
Jorge Urbina <alb15.urbi@gmail.com>
2024-09-20 20:19:39 -0600

    20/09/2024 ejemplo1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/ProductListComponent';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
+import { Ejemplo1Component } from './formularios/ejemplo1/Ejemplo1Component';
+import { CineComponent } from './formularios/cine/cine.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductoFilterPipe,
-    OperasBasComponent
+    OperasBasComponent,
+    Ejemplo1Component,
+    CineComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
